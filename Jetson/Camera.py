4f1fac808a805aa15073fe9ce07137e0408dbcf2
napiad <adam.napieralski@gmail.com>
2019-02-27 23:47:14 +0100

    object centers saved to list + objects area per full image

--- Jetson/Camera.py
+++ Jetson/Camera.py
@@ -1,6 +1,3 @@
-from ctypes import *
-import math
-import random
 import os
 import cv2
 import numpy as np
@@ -10,7 +7,8 @@ import darknet
 
 class Camera:
     label=''
-
+    # list of centers (x, y) of currently detected objects
+    objCenters = []
 
     def openCamera(self):
         
@@ -48,11 +46,10 @@ class Camera:
                 pass
 
         capture = cv2.VideoCapture(0)
-        capture.set(cv2.CAP_PROP_FRAME_WIDTH, 800)
-        capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 400)
+        # capture.set(cv2.CAP_PROP_FRAME_WIDTH, 800)
+        # capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 400)
         
-        darknet_image = darknet.make_image(darknet.network_width(netMain),
-                                    darknet.network_height(netMain),3)
+        darknet_image = darknet.make_image(darknet.network_width(netMain), darknet.network_height(netMain), 3)
 
         while True:
             stime = time.time()
@@ -63,11 +60,21 @@ class Camera:
                           (darknet.network_width(netMain),
                            darknet.network_height(netMain)),
                           interpolation=cv2.INTER_LINEAR)
-                darknet.copy_image_from_bytes(darknet_image,frame_resized.tobytes())
+
+                heightFrame = np.size(frame_resized, 0)
+                widthFrame = np.size(frame_resized, 1)
+
+                darknet.copy_image_from_bytes(darknet_image, frame_resized.tobytes())
                 
                 detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.25)
+
                 frame, xmin, ymin, xmax, ymax = self.cvDrawBoxes(detections, frame_resized)
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+                self.saveObjectsCenters(detections)
+                objectsFillLevel = self.getObjectsFillLevel(detections, heightFrame, widthFrame)
+                print("Wypelnienie:", round(objectsFillLevel, 2), "%")
+
                 cv2.imshow('frame', frame)
             print('FPS {:.1f}'.format(1 / (time.time() - stime)))
             if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -75,24 +82,69 @@ class Camera:
 
         capture.release()
         cv2.destroyAllWindows()
-        
+
+    def getObjectCenter(self, detection):
+        x, y = detection[2][0], detection[2][1]
+        return x, y
+
+    def getObjectDimensions(self, detection):
+        width, height = detection[2][2], detection[2][3]
+        return width, height
+
+    def getObjectsNum(self, detections):
+        return len(detections)
+
+    def saveObjectsCenters(self, detections):
+        objNum = self.getObjectsNum(detections)
+        for detection in detections:
+            if detections.index(detection) < len(self.objCenters):
+                self.objCenters[detections.index(detection)] = self.getObjectCenter(detection)
+            else:
+                self.objCenters.append(self.getObjectCenter(detection))
+
+        # pop all surplus elements
+        for i in range(objNum, len(self.objCenters)):
+            self.objCenters.pop(objNum)
+
+    def getObjectsVertexes(self, detections):
+        objVertexes = []
+        for detection in detections:
+            x, y = self.getObjectCenter(detection)
+            w, h = self.getObjectDimensions(detection)
+            xmin, ymin, xmax, ymax = self.convertBack(
+                float(x), float(y), float(w), float(h))
+            tl = [xmin, ymin]
+            tr = [xmax, ymin]
+            br = [xmax, ymax]
+            bl = [xmin, ymax]
+            rect = [tl, tr, br, bl]
+            objVertexes.append(rect)
+        return objVertexes
+
+    def getObjectsFillLevel(self, detections, heightFrame, widthFrame):
+        objVertexesArr = np.array(self.getObjectsVertexes(detections), dtype=np.int32)
+        im = np.zeros([heightFrame, widthFrame], dtype=np.uint8)
+        cv2.fillPoly(im, objVertexesArr, 1)
+        objectsArea = cv2.countNonZero(im)
+        frameArea = heightFrame * widthFrame
+        objFillLvl = objectsArea / frameArea * 100
+        return objFillLvl
+
     def convertBack(self, x, y, w, h):
         xmin = int(round(x - (w / 2)))
         xmax = int(round(x + (w / 2)))
         ymin = int(round(y - (h / 2)))
         ymax = int(round(y + (h / 2)))
         return xmin, ymin, xmax, ymax
-        
+
     def cvDrawBoxes(self, detections, img):
         xmin=0
         ymin=0
         xmax=0
         ymax=0
         for detection in detections:
-            x, y, w, h = detection[2][0],\
-                detection[2][1],\
-                detection[2][2],\
-                detection[2][3]
+            x, y = self.getObjectCenter(detection)
+            w, h = self.getObjectDimensions(detection)
             xmin, ymin, xmax, ymax = self.convertBack(
                 float(x), float(y), float(w), float(h))
             tl = (xmin, ymin)
@@ -108,5 +160,5 @@ class Camera:
     def getLabel(self):
         return self.label
 
-    def setLabel(self,value):
-        self.label=value
+    def setLabel(self, value):
+        self.label = value
