64eeab3dd592b57f01698cb5aa0cec023ed53904
Wincioor11 <wincioor11@gmail.com>
2019-08-13 20:52:24 +0200

    integrationThreads

--- Odroid/main-tests/MainTests.py
+++ Odroid/main-tests/MainTests.py
@@ -2,13 +2,16 @@ import time
 import threading
 import os
 from imu import IMUClass
+from MotorControl import *
+import DHT
+from connectionForTesting import *
 
 # it would be wise to use logging afterwards with more complicated code
 # import logging
 
 # logging.basicConfig(filename='output.log', level=logging.INFO)
 
-
+IP_ADDRESS = '192.168.137.22'
 from PID import PID
 
 # horizontal left / right, vertical left/mid/right
@@ -22,16 +25,19 @@ RPY_angles = [0, 0, 0]
 
 
 class DHTThread(threading.Thread):
-
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self.temp = 0
-        self.humid = 0
-
-    def run(self):
-        while True:
-            print("Temp: {}\tHumid: {}".format(self.temp, self.humid))
-            time.sleep(1.5)
+   def __init__(self):
+       threading.Thread.__init__(self)
+       self.temp = 0
+       self.humid = 0
+       self.instance = DHT.DHT11(6)
+   def run(self):
+       while True:
+           result = self.instance.read()
+           if result.is_valid():
+               self.humid = result.humidity
+               self.temp = result.temperature
+               print("Temp: {}\tHumid: {}".format(self.temp, self.humid))
+           time.sleep(6)
 
 
 class MotorsControlThread(threading.Thread):
@@ -39,35 +45,46 @@ class MotorsControlThread(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         self.lock = threading.Lock()
-        # motors = MotorControl(526)    # uncomment - check freqency, maybe put slightly different one
-        # motors.initialize_all()   # uncomment
+        self.motors = MotorControl(526)    # uncomment - check freqency, maybe put slightly different one
+        for i in range(5):
+            self.motors.initialize_motor(i)
+#self.motors.initialize_all()   # uncomment
 
     def run(self):
         while True:
             with self.lock:
                 for i in range(5):
-                    # motors.run_motor(i, motors_speed[i])    # uncomment
-                    print("{}:{}".format(motors_names[i], motors_speed[i]), end=" ")    # comment
-                print()    # comment
-                time.sleep(3)    # comment
+                    self.motors.run_motor(i, motors_speed[i])    # uncomment
+                    #print("{}:{}".format(motors_names[i], motors_speed[i]), end=" ")    # comment
+                #print()    # comment
+                #time.sleep(3)    # comment
 
 
 class IMUThread(threading.Thread):
 
-    def __init__(self):
+    def __init__(self, imu):
         threading.Thread.__init__(self)
-		self.imu = new IMUClass('roll', 'pitch', 'yaw')
+        self.lock = threading.Lock()
+        self.IMU = imu
 
-    def run(self): 
+    def run(self):
         # will start printing samples (maybe we could run it in another terminal)
-		c = 0
-		while True:
-			self.imu.catchSamples()
-			self.imu.printSamples(c % 50 == 0)
-			c += 1
-	
-	def getIMU(self):
-		return self.imu
+        #c = 0
+        while True:
+            with self.lock:
+                self.IMU.catchSamples()
+                self.connObj.setDataFrame(self.IMU.getSamples())
+                #self.IMU.printSamples(c % 50 == 0)
+                #c += 1
+
+    def getIMU(self):
+        return self.IMU
+
+    def setIMU(self, imu):
+        self.IMU = imu
+
+    def setConnection(self, connObject):
+        self.connObj = connObject
 
 
 class PIDThread(threading.Thread):
@@ -77,14 +94,20 @@ class PIDThread(threading.Thread):
         self.roll_PID = PID()
         self.pitch_PID = PID()
         self.yaw_PID = PID()
+        self.IMU = None
 
-        self.roll_diff, self.pitch_diff, self.yaw_diff = 0, 0, 0
+        self.roll_diff = 0
+        self.pitch_diff = 0
+        self.yaw_diff = 0
 
     def run(self):
         while True:
-            self.roll_diff = self.roll_PID.update(RPY_angles[0])
-            self.pitch_diff = self.pitch_PID.update(RPY_angles[1])
-            self.yaw_diff = self.yaw_PID.update(RPY_angles[2])
+            self.roll_diff = self.roll_PID.update(self.IMU.getSample('roll'))
+            self.pitch_diff = self.pitch_PID.update(self.IMU.getSample('pitch'))
+            self.yaw_diff = self.yaw_PID.update(self.IMU.getSample('yaw'))
+
+        for motor in motors_speed:
+            motor = 0
 
             self.roll_control()
             self.pitch_control()
@@ -104,6 +127,12 @@ class PIDThread(threading.Thread):
     def yaw_control(self):
         motors_speed[0] += self.yaw_diff
         motors_speed[1] -= self.yaw_diff
+
+    def getIMU(self):
+        return self.IMU
+
+    def setIMU(self, imu):
+        self.IMU = imu
 
 
 class MotorsWaitThread(threading.Thread):
@@ -169,19 +198,25 @@ class UIThread(threading.Thread):
                     motor_wait_thread.start()
 
 
-dht_thread = DHTThread()
-motors_control_thread = MotorsControlThread()
-# imu_thread = IMUThread()
+imu = IMUClass('roll', 'pitch', 'yaw')
+connThread = Connection(IP_ADDRESS)
+#dht_thread = DHTThread()
+#motors_control_thread = MotorsControlThread()
+
+imu_thread = IMUThread(imu)
+imu_thread.setConnection(connThread)
+
 # pid_thread = PIDThread()
-ui_thread = UIThread()
+#ui_thread = UIThread()
 
 # opening another terminal and executing output.log tailing
 # os.system("gnome-terminal -e 'tail -f output.log'")   # works on PC Ubuntu
 # os.system("mate-terminal --window --working-directory='~/autonomous-underwater-vehicle/Odroid/main-tests' --command='tailOutput.sh'")
 # os.system("sh -c '~/autonomous-underwater-vehicle/Odroid/main-tests/tailOutput.sh'")
 
-dht_thread.start()
-motors_control_thread.start()
-# imu_thread.start()
+#dht_thread.start()
+#motors_control_thread.start()
+imu_thread.start()
+connThread.start()
 # pid_thread.start()
-ui_thread.start()
+#ui_thread.start()
